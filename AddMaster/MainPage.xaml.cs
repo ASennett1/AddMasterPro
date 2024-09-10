2019cbbcd37a373384aea8640db69809b6406532
Andy Sennett <andy.sennett2167@fvtc.edu>
2024-09-10 16:18:29 -0700

    Adding Add Master Pro

---
+++ AddMaster/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace AddMaster;
+
+public partial class MainPage : ContentPage
+{
+    
+
+    public MainPage()
+    {
+        InitializeComponent();
+        Title = "Add Master Pro";
+    }
+
+
+    private void Clear(object sender, EventArgs e)
+    {
+        txtFirstNumber.Text = string.Empty;
+        txtSecondNumber.Text = string.Empty;
+        lblDisplay.Text = "0";
+    }
+
+    private void Add(object sender, EventArgs e)
+    {
+        double dblFirst, dblSecond;
+        double.TryParse(txtFirstNumber.Text, out dblFirst);
+        double.TryParse(txtSecondNumber.Text, out dblSecond);
+
+        lblDisplay.Text = (dblFirst + dblSecond).ToString();
+    }
+}
